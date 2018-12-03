f77c479f3f6726c1aa6672aaa42d7d5d681550df
techiefrankie <techiefrankie@gmail.com>
2018-12-03 10:35:55 +0100

    Compacted the recursive function

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ function getFactiorial(num){
  */
 
 function factorial(n){
-	if(n == 0) return 1;
-	return n * factorial(n-1);
+	return n == 0 ?  1: n * factorial(n-1);
 }
 
 /*===================
